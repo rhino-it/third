6e3719fc18976b1c43a166670ef83b87563beac8
rhino-it <it.academy.osh@gmail.com>
2019-03-11 17:39:24 +0600

    1

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -7,8 +7,8 @@
 	<link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.css';?>">
 	<link rel="stylesheet" href="<?php echo base_url().'assets/css/font-awesome.css'; ?>">
 	<link rel="stylesheet" href="<?php echo base_url().'assets/css/animate.css'; ?>">
-	<link rel="stylesheet/less" href="<?php echo base_url().'assets/css/style.less'; ?>">
-	<script src="<?php echo base_url().'assets/js/less.min.js'; ?>"></script>
+	<link rel="stylesheet" href="<?php echo base_url().'assets/css/style.css'; ?>">
+	<!-- <script src="<?php echo base_url().'assets/js/less.min.js'; ?>"></script> -->
 	<script src="<?php echo base_url().'assets/js/wow.min.js'; ?>"></script>
 	<script>
 		new WOW().init();
